612ccfd8e6c1f896fe0295a14c70d85a20ce99ec
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-30 22:20:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path =="/items"
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      
       resp.write "You correct"
     else
-      resp.write "you are lost"
+      resp.write "Route not found"
       resp.status == 404
     end
     resp.finish
