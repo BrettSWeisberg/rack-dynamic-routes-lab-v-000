01252b5cd0e99b49bab081082e19ce6e4def2194
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-30 22:25:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,17 @@
 class Application
-
+  @@items = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-
-      resp.write "You correct"
+      if items.include?(item_name) == true
+        resp.write "$3.42"
+      else
+      resp.write "Item not found"
+      resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
